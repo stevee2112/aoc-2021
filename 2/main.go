ff206b647216302d61dc28c29bfd9712cd674676
Stephen Englert <stevee2112@gmail.com>
2021-12-02 08:51:26 -0500

    Finish day 2

--- 2/main.go
+++ 2/main.go
@@ -21,7 +21,10 @@ func main() {
 	defer input.Close()
 	scanner := bufio.NewScanner(input)
 
-	sub := util.NewDirectedGraph(nil)
+	subPart1 := util.NewDirectedGraph(nil)
+	subPart2 := util.NewDirectedGraph(nil)
+	aim := 0
+
 	for scanner.Scan() {
 		instruction := scanner.Text()
 
@@ -35,15 +38,27 @@ func main() {
 			compassDir = util.East
 		case "up":
 			compassDir = util.North
+			aim -= value
 		case "down":
 			compassDir = util.South
+			aim += value
 		}
 
 		for i := 0; i < value; i++ {
-			sub.Move(compassDir)
+			subPart1.Move(compassDir)
+		}
+
+		if direction == "forward" {
+			for i := 0; i < value; i++ {
+				subPart2.Move(util.East)
+			}
+
+			for i := 0; i < (aim * value); i++ {
+				subPart2.Move(util.South)
+			}
 		}
 	}
 
-	fmt.Printf("Part 1: %d\n", util.Abs(sub.At().X) * util.Abs(sub.At().Y))
-	fmt.Printf("Part 2: %d\n", 0)
+	fmt.Printf("Part 1: %d\n", util.Abs(subPart1.At().X) * util.Abs(subPart1.At().Y))
+	fmt.Printf("Part 2: %d\n", util.Abs(subPart2.At().X) * util.Abs(subPart2.At().Y))
 }
