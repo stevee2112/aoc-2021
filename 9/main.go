d7d0b601758cb25753a397d65fa01c799c8e2563
Stephen Englert <stevee2112@gmail.com>
2021-12-09 17:52:48 -0500

    finish day 9

--- 9/main.go
+++ 9/main.go
@@ -7,8 +7,8 @@ import (
 	"path"
 	"runtime"
 	"stevee2112/aoc-2021/util"
-	//"strconv"
 	"strings"
+	"sort"
 )
 
 func main() {
@@ -36,6 +36,7 @@ func main() {
 
 	// Part 1
 	sum := 0
+	lowPoints := []util.Coordinate{}
 	heatMap.Traverse(func(coor util.Coordinate) bool {
 		adjacent := heatMap.GetAdjacent(coor)
 
@@ -48,12 +49,50 @@ func main() {
 		}
 
 		if isLowest {
+			lowPoints = append(lowPoints, coor)
 			sum += (1 + coor.Value.(int))
 		}
 
 		return true
 	})
 
+	// Part2
+	basins := []int{}
+	for _,lowPoint := range lowPoints {
+		basins = append(basins, getBasinSize(heatMap, lowPoint))
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
+
+	part2Answer := basins[0] * basins[1] * basins[2]
+
 	fmt.Printf("Part 1: %d\n", sum)
-	fmt.Printf("Part 2: %d\n", 0)
+	fmt.Printf("Part 2: %d\n", part2Answer)
+}
+
+func getBasinSize(heatMap util.Grid, coor util.Coordinate) int {
+
+	inBasin := getCoordinatesInBasin(heatMap, coor, map[string]bool{})
+	return len(inBasin)
+}
+
+func getCoordinatesInBasin(heatMap util.Grid, coor util.Coordinate, alreadyVisited map[string]bool) []util.Coordinate {
+
+	alreadyVisited[coor.String()] = true
+
+	coordinates := []util.Coordinate{coor}
+
+	for _,a := range heatMap.GetAdjacent(coor) {
+		if a.Value == 9 {
+			continue
+		} else {
+			if _,found := alreadyVisited[a.String()]; found {
+				continue // skip if we cam from this coordinate
+			}
+
+			coordinates = append(coordinates, getCoordinatesInBasin(heatMap, a, alreadyVisited)...)
+		}
+	}
+
+	return coordinates
 }
