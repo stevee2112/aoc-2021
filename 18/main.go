9e09f73c989117f346031a7aa985b204e2717d29
Stephen Englert <stevee2112@gmail.com>
2021-12-20 12:27:18 -0500

    slow progress

--- 18/main.go
+++ 18/main.go
@@ -33,14 +33,132 @@ func main() {
 		//_ := scanner.Text()
 	}
 
-	pairStr := "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
+	pairStr := "[1,[[3,[2,[1,[7,3]]]],[6,1]]]" // TODO THIS IS NOT WORKING
+	// need to better handle getting left and right.  I think the better way
+	// is to traverse like the print function which is seeing nodes in order
+	// then when we match getting the previous, and next.
 
 	pair := parsePair(pairStr)
-	fmt.Println(pairStr)
+
+	fmt.Println(printPair(pair))
+	fmt.Println(explode(pair))
 	fmt.Println(printPair(pair))
 
 	fmt.Printf("Part 1: %d\n", 0)
 	fmt.Printf("Part 2: %d\n", 0)
+}
+
+func getDepth(current int, pair *Pair) (depth int) {
+
+	xDepth := current
+	if pair.xPair != nil {
+		xDepth = getDepth(current + 1, pair.xPair)
+	}
+
+	yDepth := current
+	if pair.yPair != nil {
+		yDepth = getDepth(current + 1, pair.yPair)
+	}
+
+	max := xDepth
+
+	if yDepth > max {
+		max = yDepth
+	}
+
+	return max
+}
+
+func getFirstDepth4(current int, pair *Pair) (depth int, fpair *Pair) {
+
+	fpair = pair
+    xDepth := current
+    if pair.xPair != nil {
+        xDepth,fpair = getFirstDepth4(current + 1, pair.xPair)
+
+		if xDepth >= 4 {
+			return xDepth,fpair
+		}
+    }
+
+    yDepth := current
+    if pair.yPair != nil {
+        yDepth,fpair = getFirstDepth4(current + 1, pair.yPair)
+
+		if xDepth >= 4 {
+			return xDepth,fpair
+		}
+    }
+
+    max := yDepth
+
+    if xDepth > max {
+        max = xDepth
+    }
+
+    return max,fpair
+}
+
+func explode(pair *Pair) (bool, *Pair) {
+
+	if getDepth(0, pair) < 4 {
+		return false, pair
+	}
+
+	_,exploded := getFirstDepth4(0,pair)
+
+	xPlode := exploded.xLiteral
+	yPlode := exploded.yLiteral
+
+	leftAt := exploded.parent
+	rightAt := exploded.parent
+
+	// set left value
+	for leftAt != nil {
+		if leftAt.xPair != nil {
+			leftAt = leftAt.parent
+		} else {
+			leftAt.xLiteral += xPlode
+			leftAt.xPair = nil
+			break
+		}
+	}
+
+	// set right value
+	for rightAt != nil {
+		if rightAt.yPair != nil {
+			fmt.Println(rightAt)
+			rightAt = rightAt.parent
+		} else {
+			rightAt.yLiteral += yPlode
+			rightAt.yPair = nil
+			break
+		}
+	}
+
+	// replace with 0
+	parent := exploded.parent
+
+	if parent.xPair == exploded {
+		parent.xPair = nil
+	}
+
+	if parent.yPair == exploded {
+		parent.yPair = nil
+	}
+
+
+
+	return true, pair
+}
+
+func addPairs(a *Pair, b *Pair) *Pair {
+	var pair *Pair
+	new := Pair{}
+	pair = &new
+	pair.xPair = a
+	pair.yPair = b
+	return pair
 }
 
 func parsePair(p string) *Pair {
